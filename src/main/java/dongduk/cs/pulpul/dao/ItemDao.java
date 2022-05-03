6eb00702ce2c0e1b86628bbb191cab2d9ab0f078
yuriLee <yurilee07@naver.com>
2022-05-03 17:28:47 +0900

    dao interface 파일 생성(1/2)

---
+++ src/main/java/dongduk/cs/pulpul/dao/ItemDao.java
@@ -0,0 +1,40 @@
+package dongduk.cs.pulpul.dao;
+
+import java.util.List;
+
+public interface ItemDao {
+	/*
+	// 전체 상품 목록 조회
+	public List<Goods> findAllGoods();
+	
+	// 회원 id로 상품 목록 조회
+	public List<Goods> findGoodsByMember(String memberId);
+	
+	// 마켓 id로 상품 목록 조회
+	public List<Goods> findGoodsByMarket(int marketId);
+	
+	// 새 상품 목록 조회
+	public List<Goods> findNewGoods();
+	
+	// 키워드로 상품 목록 조회
+	public List<Goods> findGoodsByKeyword(String keyword);
+	
+	// 품목 id로 상품 목록 조회
+	public Goods findGoodsByItem(String itemId);
+	
+	// 상품 생성
+	public boolean createGoods(Goods goods);
+	
+	// 상품 정보 수정
+	public boolean changeGoodsInfo(Goods goods);
+	
+	// 상품 판매 수량 변경
+	public boolean changeSalesQuantity(Goods goods);
+	
+	// 주문 상태에 따라 남은 상품 수량 변경
+	public boolean changeRemainQuantityByOrderStatus(int orderId, int quantity);
+	
+	// 상품에 대한 주문 내역 존재여부 확인
+	public boolean isExistOrder(String itemId);
+	*/
+}
---
+++ src/main/java/dongduk/cs/pulpul/dao/MemberDao.java
@@ -0,0 +1,20 @@
+package dongduk.cs.pulpul.dao;
+
+public interface MemberDao {
+	/*
+	// 회원 id로 회원 정보 조회
+	public Member findMember(String memberId);
+	
+	// 회원 정보 생성
+	public boolean createMember(Member member);
+	
+	// 회원 정보 수정
+	public boolean changeMemberInfo(Member member);
+	
+	// 회원 정보 삭제
+	public boolean deleteMember(Member member);
+	
+	// 회원 포인트 값 변경
+	public boolean changePoint();
+	*/
+}
