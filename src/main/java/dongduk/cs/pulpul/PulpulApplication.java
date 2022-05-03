b358efdd94ac4d91ab13226fd73a423e5cad66a7
duldul34 <duldul34@users.noreply.github.com>
2022-05-03 15:21:12 +0900

    first commit

---
+++ src/main/java/dongduk/cs/pulpul/PulpulApplication.java
@@ -0,0 +1,13 @@
+package dongduk.cs.pulpul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulpulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulpulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dongduk/cs/pulpul/PulpulApplicationTests.java
@@ -0,0 +1,13 @@
+package dongduk.cs.pulpul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulpulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
