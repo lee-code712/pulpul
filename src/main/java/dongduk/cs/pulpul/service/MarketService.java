d2ef28a6b305e21a9edfae6e26a3c4ded8fcbbf6
yuriLee <yurilee07@naver.com>
2022-05-04 18:03:46 +0900

    service interface 파일 생성(1/2)

---
+++ src/main/java/dongduk/cs/pulpul/service/MarketService.java
@@ -0,0 +1,17 @@
+package dongduk.cs.pulpul.service;
+
+public interface MarketService {
+	/*
+	// 마켓 id로 마켓 조회
+	Market getMarket(int marketId);
+	
+	// 회원 id로 마켓 조회
+	Market getMarketByMember(String memberId);
+	
+	// 마켓 생성
+	boolean makeMarket(Market market);
+	
+	// 마켓 정보 수정
+	boolean changeMarketInfo(Market market);
+	*/
+}
---
+++ src/main/java/dongduk/cs/pulpul/service/ReviewService.java
@@ -0,0 +1,16 @@
+package dongduk.cs.pulpul.service;
+
+import java.util.List;
+
+public interface ReviewService {
+	/*
+	// 품목 id로 리뷰 목록 조회
+	List<Review> getReviewByItem(String itemId);
+	
+	// 리뷰할 주문 존재여부 확인
+	List<String> checkNotExistReview(String itemId);
+	
+	// 리뷰 작성
+	boolean addReview(Review review);
+	*/
+}
