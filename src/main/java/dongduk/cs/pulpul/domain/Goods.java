765967e91f6dab35c03e5414894d2d55a381fa03
yoonkyung <kyung3098@naver.com>
2022-05-17 00:52:03 +0900

    valid annotation 임의로 추가

--- src/main/java/dongduk/cs/pulpul/domain/Goods.java
+++ src/main/java/dongduk/cs/pulpul/domain/Goods.java
@@ -1,15 +1,26 @@
 package dongduk.cs.pulpul.domain;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Goods {
 
 	private Item item; /*상품 품목 정보*/
+	@NotBlank
 	private String tags; /*상품 태그*/
 	private int price;
+	@NotBlank
 	private String size;
+	
+	@PositiveOrZero(message = "수량은 0개 이상이어야 합니다.")
 	private int salesQuantity; /*총 판매 수량*/
 	private int remainQuantity; /*남은 수량*/
 	private int shippingFee; /*배송비*/
