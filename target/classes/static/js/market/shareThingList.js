1178dcbf1dccfafd2c30e8296b8b6e815a995023
yoonkyung <kyung3098@naver.com>
2022-05-18 20:18:37 +0900

    공유물품 목록

---
+++ target/classes/static/js/market/shareThingList.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+ 
+ function encodeUri(uri){
+	const encoded = encodeURI(uri);
+	location.href = encoded;
+}
+ function findBorrowShareThing(id){
+	const uri = '/market/shareThingBorrowManage?itemId=' + id;
+	encodeUri(uri);
+}
+
+function deleteShareThing(id){
+	const uri = '/market/shareThing/delete?itemId=' + id;
+	encodeUri(uri);
+}
