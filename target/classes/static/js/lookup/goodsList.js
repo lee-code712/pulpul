072f7e45ce1735ff6c8b1ff445ed7c75ec06110d
yoonkyung <kyung3098@naver.com>
2022-05-25 13:32:42 +0900

    식물 조회 - 태그로도 검색 가능

--- target/classes/static/js/lookup/goodsList.js
+++ target/classes/static/js/lookup/goodsList.js
@@ -10,6 +10,16 @@
 	encodeUri(uri);
 }
 
+//태그로 검색
+function searchTag(value){
+	
+		$(".tag-name:contains('"+value+"')").each(function() {
+		  var $this = $(this);
+		  $this.parents(".item").show();
+
+		});
+}
+
 //검색 필터
 jQuery(function($){ $('.filterText').keyup(function(event) { 
 	var val = $(this).val(); 
@@ -17,7 +27,36 @@ jQuery(function($){ $('.filterText').keyup(function(event) {
 	     $('.item').show();
 	 }else {
 		 $('.item').hide(); 
- 		 $(".item-name:contains('"+val+"')").parent('.item').show();
+		
+		//태그로 검색
+		searchTag(val);
+		
+		//식물 이름으로 검색
+		if($(".item-name:contains('"+val+"')")){
+			$(".item-name:contains('"+val+"')").parent('.item').show();
+		}
+		
  	  }
    });
  });
+
+//태그 클릭 시 이벤트 처리
+function onClickTag(event){
+	var tag = event.target.innerHTML;
+	var val = $('input[id=itemSearch]').val(tag);
+	
+	if (tag == "") {
+	     $('.item').show();
+	 }else {
+		 $('.item').hide(); 
+	
+	//태그로 검색
+	 searchTag(tag);
+		
+	}
+}
+
+const tags = document.querySelectorAll('.search-tag');
+tags.forEach( (tag) => {
+  tag.addEventListener('click', onClickTag)
+});
