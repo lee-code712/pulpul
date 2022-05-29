db84b27fd6293f9a8ca20f040b3b5ca36074b88f
hr_0513 <hr_0513@naver.com>
2022-05-30 08:08:20 +0900

    shareThingList 검색 기능 구현

--- target/classes/static/js/lookup/shareThingList.js
+++ target/classes/static/js/lookup/shareThingList.js
@@ -1,11 +1,22 @@
-/**
- * 
- */
- function encodeUri(uri){
+// uri
+function encodeUri(uri){
 	const encoded = encodeURI(uri);
 	location.href = encoded;
 }
- function moveToShareThingDetail(id){
+
+// 공유 상세정보로 이동
+function moveToShareThingDetail(id){
 	const uri = '/lookup/shareThingDetail?itemId=' + id;
 	encodeUri(uri);
 }
+
+// 검색필터
+$(document).ready(function(){
+  $("#itemSearch").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    
+    $(".item").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
