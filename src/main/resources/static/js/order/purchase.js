c599f4b3dece7641f10a52a18c2ed9022bd7d0ac
yoonkyung <kyung3098@naver.com>
2022-06-02 23:40:58 +0900

    결제 화면 수정 total-price hidden값 넘겨주기, 잔여 포인트 보다 큰 포인트 입력 안되게

--- src/main/resources/static/js/order/purchase.js
+++ src/main/resources/static/js/order/purchase.js
@@ -2,19 +2,30 @@
  * 
  */
  //총 상품 금액 계산
- var totalPrice = $("#total-price").text().replace(",", "").split("원")[0]; //총 결제 금액
- var totalShipping = $("#total-shipping").text().replace(",", "").split("원")[0]; //배송비
- var itemPrice = Number(totalPrice) - Number(totalShipping);
+
+ var totalPrice = Number($("#total-price").text().replace(",", "").split("원")[0]); //총 결제 금액
+ $("#total-input").attr("value", totalPrice.toString()); //total-price input hidden
+ var totalShipping = Number($("#total-shipping").text().replace(",", "").split("원")[0]); //배송비
+ var itemPrice;
+ var point = Number($("#point").text());
+ 
+if(point > 0) {
+	$("#total-price").text(totalPrice - point);
+	itemPrice = totalPrice - totalShipping + point;
+}else{
+	itemPrice = totalPrice - totalShipping;
+}
  $("#total-item").text(itemPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "원");
  
  //잔여 포인트
- var leftover = $("#leftover").text().replace(",", "").replace("잔여", "").split("원")[0];
+ var leftover = $("#leftover").text().replace(",", "").replace("잔여 포인트", "").split("원")[0];
  
  $("#point").keyup(function(){
+	
 	var point = Number($(this).val());
 	var total;
+	if(point > Number(leftover)){ //잔여 포인트보다 큰 값 입력시
 	
-	if(point > leftover){ //잔여 포인트보다 큰 값 입력시
 		$(this).val("");
 		total = totalPrice;
 		$("#leftover").css("color", "red");
@@ -24,6 +35,7 @@
 	}
 	
 	$("#total-price").text(total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "원");
+	$("#total-input").attr("value", total.toString());
 })
 
  
@@ -67,7 +79,15 @@ function autoHypenCard(str){
     return str;
 }
 
- $("#validDate").keyup(function(){;
+/* 카드 유효기간 */
+ $("#validDate").keyup(function(){
+	
+	if($(this).val().length == 2){
+		if(Number($(this).val()) > 12 || Number($(this).val()) < 1){
+			$(this).val("");
+		}
+	}
+	
     var _slash = $(this).val();
         _slash = _slash.replace(/(\d{2})(\d{2})/, '$1/$2');
     $(this).val(_slash);
