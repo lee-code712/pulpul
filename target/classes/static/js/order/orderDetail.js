8bb588b25f10198ce2a6cd92e33ce40f60b9de2e
yoonkyung <kyung3098@naver.com>
2022-06-04 21:01:30 +0900

    orderDetail js 추가

---
+++ target/classes/static/js/order/orderDetail.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+function encodeUri(uri){
+	const encoded = encodeURI(uri);
+	location.href = encoded;
+}
+ function moveToGoodsDetail(id){
+	const uri = '/lookup/goodsDetail?itemId=' + id;
+	encodeUri(uri);
+}
+
+//배송비 계산
+ var totalItem = $("#total-item").text().replace(",", "").split("원")[0];
+ var totalPrice = Number($("#totalPrice").text().replace(",", "").split("원")[0]);
+ var shipping = $("#total-shippingFee");
+ if($("#point")){
+	shipping.text((totalPrice - totalItem).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "원");
+	}else{
+		var point = Number($("#point").text().replace(",", "").split("원")[0]);
+		shipping.text((totalPrice - totalItem + point).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "원");	
+}
