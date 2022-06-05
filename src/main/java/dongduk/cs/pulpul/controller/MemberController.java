800509d94ce814d17aec1f27a970a1786711f5c5
duldul34 <duldul34@users.noreply.github.com>
2022-06-05 14:26:37 +0900

    마이페이지 컨트롤러 json 반환하도록 수정

--- src/main/java/dongduk/cs/pulpul/controller/MemberController.java
+++ src/main/java/dongduk/cs/pulpul/controller/MemberController.java
@@ -1,8 +1,10 @@
 package dongduk.cs.pulpul.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +15,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import dongduk.cs.pulpul.domain.Borrow;
+import dongduk.cs.pulpul.domain.Market;
 import dongduk.cs.pulpul.domain.Member;
 import dongduk.cs.pulpul.domain.Order;
 import dongduk.cs.pulpul.service.BorrowService;
@@ -212,60 +217,71 @@ public class MemberController {
 		 */
 	}
 	
-	/*
-	 * 마이 페이지 접근 - 구매 목록 조회
-	 */
-	@GetMapping("/mypage/orderList")
-	public ModelAndView myOrderList(HttpServletRequest req, ModelAndView mav) {
+	@GetMapping("/mypage")
+	public String mypage(HttpServletRequest req, Model model) {
 		//마이 페이지
 		HttpSession session = req.getSession();
 		String id = (String) session.getAttribute("id");
 		
 		// 로그인한 상태인지 확인
 		if (id == null) {
-			mav.addObject("isNotLogined", true);
-			mav.setViewName("redirect:/home");
-			return mav;
+			model.addAttribute("isNotLogined", true);
+			return "redirect:/home";
 		}
+		
+		return "member/mypage";
+	}
+	
+	/*
+	 * 마이 페이지 접근 - 구매 목록 조회
+	 */
+	@GetMapping("/mypage/orderList")
+	@ResponseBody
+	public List<Order> myOrderList(HttpServletRequest req, HttpServletResponse response) throws IOException {
+		//마이 페이지
+		HttpSession session = req.getSession();
+		String id = (String) session.getAttribute("id");
+		
+		/*
+		 * // 로그인한 상태 확인 if (id == null) { mav.addObject("isNotLogined", true);
+		 * mav.setViewName("redirect:/home"); return mav; }
+		 */
 		
 		List<Order> orderList = orderService.getOrderListByMember(id, "buyer");
-		if (orderList != null) {
-			mav.addObject("orderList", orderList);
+		if (orderList == null) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
 		}
 		
-		mav.setViewName("member/mypage");
-		return mav;
+		return orderList;
 	}
 	
 	/*
 	 * 마이 페이지 접근 - 대여, 예약 목록 조회
 	 */
 	@GetMapping("/mypage/borrowList")
-	public ModelAndView myBorrowList(HttpServletRequest req, ModelAndView mav) {
+	@ResponseBody
+	public List<Borrow> myBorrowList(HttpServletRequest req, HttpServletResponse response) throws IOException {
 		HttpSession session = req.getSession();
 		String id = (String) session.getAttribute("id");
 		
 		List<Borrow> borrowList = borrowService.getBorrowByMember(id, "borrower");
 
-		if (borrowList != null) {
-			
-			for (Borrow borrow : borrowList) {
-				System.out.println(borrow.getId());
-			}
-			mav.addObject("borrowList", borrowList);
+		if (borrowList == null) {
+			// response.sendError(HttpServletResponse.SC_NOT_FOUND);
 		}
 		
 		List<Borrow> reservationList = borrowService.getBorrowReservationByMember(id);
 		
-		if (reservationList != null) {
-			for (Borrow borrow : reservationList) {
-				System.out.println(borrow.getId());
+		if (reservationList == null) {
+			// response.sendError(HttpServletResponse.SC_NOT_FOUND);
+		}
+		else {
+			for (Borrow r : reservationList) {
+				borrowList.add(r);
 			}
-			mav.addObject("reservationList", reservationList);
 		}
 		
-		mav.setViewName("member/mypage");
-		return mav;
+		return borrowList;
 	}
 
 
