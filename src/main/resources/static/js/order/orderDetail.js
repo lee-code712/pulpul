c9e56c5ec0dd07ca9044f7f7e76c1d83ae700682
yoonkyung <kyung3098@naver.com>
2022-06-09 01:04:37 +0900

    배송비 수정, 카드사 카드 번호 추가

--- src/main/resources/static/js/order/orderDetail.js
+++ src/main/resources/static/js/order/orderDetail.js
@@ -15,9 +15,11 @@ function encodeUri(uri){
  var totalItem = $("#total-item").text().replace(",", "").split("원")[0];
  var totalPrice = Number($("#totalPrice").text().replace(",", "").split("원")[0]);
  var shipping = $("#total-shippingFee");
- if(!$("#point")){
+
+var itemNum = $(".item-name");
+	if(!$("#point")){
 	shipping.text("+" + (totalPrice - totalItem).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "원");
 	}else{
-		var point = Number($("#point").text().replace(",", "").split("원")[0]);
+		var point = -Number($("#point").text().replace(",", "").split("원")[0]);
 		shipping.text("+" + (totalPrice - totalItem + point).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "원");	
 }
