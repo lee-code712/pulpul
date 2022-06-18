ac881b40359ecac10a044da4b8ccbc08e8585475
yoonkyung <kyung3098@naver.com>
2022-06-19 00:22:49 +0900

    운송장 번호 Get parameter로 전달 검색 이미지 클릭

--- target/classes/static/js/market/orderListManage.js
+++ target/classes/static/js/market/orderListManage.js
@@ -2,18 +2,26 @@
  * 
  */
  
- //검색 필터
-jQuery(function($){ $('.filterText').keyup(function(event) { 
-	var val = $(this).val(); 
- 	if (val == "") {
-	     $('.order').show();
-	 }else {
-		 $('.order').hide(); 
-		if($(".trackingNumber:contains('"+val+"')")){
-			$(".trackingNumber:contains('"+val+"')").parents('.order').show();
-		}
-		
- 	  }
-   });
- });
+ if($('tbody').children().length == 1){ //해당 운송장 번호에 주문 내역이 없는 경우
+	Swal.fire({
+				  text: "해당 주문내역이 없습니다.",
+    			  confirmButtonColor: '#93c0b5',
+				  confirmButtonText: '확인',
+				  }).then((result) => {
+				  if (result.isConfirmed) { 
+						location.href="/market/orderListManage";
+				  }
+			  	});
+}
+ 
+function encodeUri(uri){
+	const encoded = encodeURI(uri);
+	location.href = encoded;
+}
+
+ function searchTrackingNumber(){
+	let trackingNumber = $('.filterText').val();
+	const uri = '/market/orderListManage?trackingNumber=' + trackingNumber;
+	encodeUri(uri);
+}
 
