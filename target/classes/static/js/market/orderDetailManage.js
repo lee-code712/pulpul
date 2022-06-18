140af5e3446dea5fef16badb1d8c0c20e45344a8
yoonkyung <kyung3098@naver.com>
2022-06-19 00:27:18 +0900

    결제 상품 상세정보와 똑같이 변경

---
+++ target/classes/static/js/market/orderDetailManage.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+
+function encodeUri(uri){
+	const encoded = encodeURI(uri);
+	location.href = encoded;
+}
+ function moveToGoodsDetail(id){
+	const uri = '/lookup/goodsDetail?itemId=' + id;
+	encodeUri(uri);
+}
