5c68d7604e4ddd19edada899e28c1ee4e5107c68
yoonkyung <kyung3098@naver.com>
2022-06-21 14:23:51 +0900

    운송장 번호 검색 오류 수정

--- target/classes/static/js/market/orderList.js
+++ target/classes/static/js/market/orderList.js
@@ -1,19 +1,24 @@
 /**
  * 
  */
+ let url = window.location.href;
  
- if($('tbody').children().length == 1){ //해당 운송장 번호에 주문 내역이 없는 경우
-	Swal.fire({
-				  text: "해당 주문내역이 없습니다.",
-    			  confirmButtonColor: '#93c0b5',
-				  confirmButtonText: '확인',
-				  }).then((result) => {
-				  if (result.isConfirmed) { 
-						location.href="/market/orderList";
-				  }
-			  	});
-}
-
+ if($('tbody').children().length == 1){ //주문 내역이 없을 때
+    $("#totalPrice").text("0원");
+    if(url.includes("trackingNumber")){ //해당 운송장 번호 검색시 주문 내역이 없는 경우
+		Swal.fire({
+					  text: "해당 주문내역이 없습니다.",
+	    			  confirmButtonColor: '#93c0b5',
+					  confirmButtonText: '확인',
+					  }).then((result) => {
+					  if (result.isConfirmed) { 
+							location.href="/market/orderList";
+					  }
+				  	});
+		
+		 }
+	 }
+	
  function searchTrackingNumber(){
 	let trackingNumber = $('.filterText').val();
 	if(trackingNumber == ''){ //입력 안하고 검색했을 때
@@ -27,6 +32,7 @@
 			  	});
 			  	return;
 	}
+	
 	const uri = '/market/orderList?trackingNumber=' + trackingNumber;
 	encodeUri(uri);
 }
